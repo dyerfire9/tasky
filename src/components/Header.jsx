eaa6feaec7f0ecad135f738a7bb1130848e70619
my name <my email address>
2023-12-29 21:23:11 -0500

    Created Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <div className='header'>
+        <h2 className='title'>Tasky</h2>
+        <button className='add-btn'>Add</button>
+    </div>
+  )
+}
+
+export default Header
